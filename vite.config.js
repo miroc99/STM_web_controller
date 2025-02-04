0f55dbc9b13d4d7dcbc18d1567196e6ad02bfe98
miroc_whyixd_desktop <miroc@whyicxd.com>
2025-02-04 11:42:40 +0800

    set to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default {
+  base: "/STM_web_controller/",
   root: resolve(__dirname, "src"),
   build: {
-    outDir: "../dist",
+    outDir: "dist",
+    assetsDir: "assets",
   },
   server: {
     port: 8080,
